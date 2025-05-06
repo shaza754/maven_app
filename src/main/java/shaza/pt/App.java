aaf707729117bc97e09d092e2b6fb40615ac1fb7
pathan shajahan <pathanshajahankhan@gmail.com>
2025-05-06 11:58:02 +0530

    first commit

---
+++ src/main/java/shaza/pt/App.java
@@ -0,0 +1,13 @@
+package shaza.pt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
